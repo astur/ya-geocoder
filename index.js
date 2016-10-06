7afa680daa4047152d7cd846f55123f45b9b440b
astur <astur@yandex.ru>
2016-10-06 09:01:52 +0300

    first working version

--- index.js
+++ index.js
@@ -18,6 +18,55 @@ module.exports = function(address, callback){
 
         var bld = {};
 
+        var _ = res.body.response.GeoObjectCollection.featureMember[0].GeoObject;
+
+        bld.pos = {
+            type: 'Point',
+            coordinates: _.Point.pos.split(' ').map(v=>+v),
+        };
+
+        bld.boundedBy = {
+            type: 'MultiPoint',
+            coordinates: [
+                _.boundedBy.Envelope.lowerCorner.split(' ').map(v=>+v),
+                _.boundedBy.Envelope.upperCorner.split(' ').map(v=>+v),
+            ],
+        }
+
+        _ = _.metaDataProperty.GeocoderMetaData;
+
+        bld.kind = _.kind;
+        bld.precision = _.precision;
+
+        _ = _.AddressDetails.Country;
+
+        bld.address = _.AddressLine;
+
+        if(_.hasOwnProperty('AdministrativeArea')){
+            _ = _.AdministrativeArea;
+            bld.area = _.AdministrativeAreaName;
+        }
+
+        if(_.hasOwnProperty('SubAdministrativeArea')){
+            _ = _.SubAdministrativeArea;
+            bld.region = _.SubAdministrativeAreaName;
+        }
+
+        if(_.hasOwnProperty('Locality')){
+            _ = _.Locality;
+            bld.city = _.LocalityName;
+        }
+
+        if(_.hasOwnProperty('Thoroughfare')){
+            _ = _.Thoroughfare;
+            bld.street = _.ThoroughfareName;
+        }
+
+        if(_.hasOwnProperty('Premise')){
+            _ = _.Premise;
+            bld.bldNum = _.PremiseNumber;
+        }
+
         callback(null, bld);
     });
 }
